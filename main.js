b7950edcbdbc63e0b3c66b24dd7b41e219920cc6
unknown <leongit1990@gmail.com>
2020-06-08 05:47:49 +0200

    Completed and ready to compile. See packagejson scripts

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 //App: manages the lifecycle of the app
 //Browser window creates windows
-const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+const os = require('os');
+const { app, BrowserWindow, Menu, globalShortcut, ipcMain, shell } = require('electron');
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg')
+const imageminPngquant = require('imagemin-pngquant')
+const slash = require('slash');
+const log = require('electron-log');
+
 
 //Set env
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = 'production';
 
 const isDev = process.env.NODE_ENV !== 'production' ? true : false;
 const isMac = process.platform === 'darwin' ? true : false;
 
-let main;
+let mainWindow;
+let aboutWindow;
 
 function createMainWindow() {
     mainWindow = new BrowserWindow({
@@ -16,10 +25,25 @@ function createMainWindow() {
         width: 500,
         height: 600,
         icon: `${__dirname}/app/icons/Icon_256x256.png`,
-        resizable: isDev
+        resizable: isDev,
+        webPreferences: {
+            nodeIntegration: true
+        }
     });
 
     mainWindow.loadFile('./app/index.html');
+}
+
+function createAboutWindow() {
+    aboutWindow = new BrowserWindow({
+        title: 'About ImageShrink',
+        width: 300,
+        height: 300,
+        icon: `${__dirname}/app/icons/Icon_256x256.png`,
+        resizable: isDev
+    });
+
+    aboutWindow.loadFile('./app/about.html');
 }
 
 
@@ -29,23 +53,84 @@ app.on('ready', () => {
     const mainMenu = Menu.buildFromTemplate(menu);
     Menu.setApplicationMenu(mainMenu);
 
+    /*
+    globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
+    globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools())
+    */
+
     mainWindow.on('closed', () => mainWindow = null);
 });
 
 const menu = [
     ...(isMac ? [
-        { role: 'appMenu'}
+        {
+            label: app.name,
+            submenu: [
+                {
+                    label: 'About',
+                    click: createAboutWindow
+                }
+            ]
+        }
     ] : []),
     {
-        label: 'File',
-        submenu: [
-            {
-                label: 'Quit',
-                click: () => app.quit()
-            }
-        ]
+        role: 'fileMenu'
+    },
+    ...(!isMac ? [
+        {
+            label: 'Help',
+            submenu: [
+                {
+                    label: 'About',
+                    click: createAboutWindow
+                }
+            ]
+        }
+    ] : []),
+    ...(isDev ? [
+        {
+            label: 'Developer',
+            submenu: [
+                {role: 'reload'},
+                {role: 'forcereload'},
+                {role: 'separator'},
+                {role: 'toggledevtools'}
+            ]
+        }
+    ] : [])
+];
+
+
+ipcMain.on('image:minimize', (e, options) => {
+    options.dest = path.join(os.homedir(), 'imageShrink');
+    shrinkImage(options)
+})
+
+async function shrinkImage({ imgPath, quality, dest}) {
+    try {
+        const pngQuality = quality / 100;
+
+        const files = await imagemin([slash(imgPath)], {
+            destination: dest,
+            plugin: [
+                imageminMozjpeg({ quality }),
+                imageminPngquant({
+                    quality: [pngQuality, pngQuality]
+                })
+            ]
+        })
+
+
+
+        shell.openPath(dest);
+        log.info(files)
+        //Send stuff to the renderer. Similar to websockets
+        mainWindow.webContents.send('image:done')
+    } catch (err) {
+        log.error(err)
     }
-]
+
+}
 
 
 
